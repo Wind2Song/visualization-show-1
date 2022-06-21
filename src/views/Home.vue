d203ca70309f0b62454861c6883cbf5f447ed628
HCgittt <3538371984@qq.com>
2022-06-21 14:36:50 +0800

    init&directives

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img v-x:click="woshini" alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+    directives:{
+    'x':{
+      inserted:function(el,info){
+        console.log(info)
+        el.addEventListener(info.arg,info.value)
+      }
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods:{
+    woshini(){console.log('hi')}
+  }
+}
+</script>
